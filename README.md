34b4a2add4ee1f8d26674be0cd80e6874254ee85
twelsh <twelsh37@gmail.com>
2024-01-15 21:59:09 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This project is to create a  dash application that allows a user to upload or drag and drop both a CV and a job decription into the interface. It will then match skills on the CV and skills in the Job description.<p>
+One of the outputs will be a radar graph showing how the Applicant CV matches the Job Description.<p>
+
+Future tasks include:<p>
+1. Provide a similarity score as a percentage of how good a match particular match a CV is to a Job Description.<p>
+2. Provide a list of skills that are missing from the CV that are in the Job Description.<p>
+3. Provide a list of skills that are missing from the Job Description that are in the CV.<p>
+4. Provide a list of skills that are in both the CV and the Job Description.<p>
+5. Create a database of CVs and Job Descriptions that can be used to train a machine learning model to match CVs to Job Descriptions.<p>
+
+Currently we are starting from here and will build up the functionality as we go.<p>
+
+![img.png](img.png)
---
+++ app.py
@@ -0,0 +1,157 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
+import docx2txt
+import PyPDF2
+from io import BytesIO
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from collections import Counter
+import base64
+
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-cv',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select a CV')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=False
+    ),
+    dcc.Textarea(
+        id='cv-text',
+        placeholder='CV text will appear here...',
+        style={'width': '100%', 'height': '200px'},
+    ),
+    dcc.Textarea(
+        id='job-description',
+        placeholder='Enter job description URL or text here...',
+        style={'width': '100%', 'height': '200px'},
+    ),
+    html.Button('Analyze', id='analyze-button', n_clicks=0),
+    dcc.Graph(id='radar-graph')
+])
+
+
+def extract_text_from_cv(contents, filename):
+    """
+    Extract text from CV in Word or PDF format.
+    """
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'doc' in filename:
+            text = docx2txt.process(BytesIO(decoded))
+        elif 'pdf' in filename:
+            reader = PyPDF2.PdfFileReader(BytesIO(decoded))
+            text = ' '.join([reader.getPage(i).extractText() for i in range(reader.getNumPages())])
+        else:
+            raise Exception('Invalid file type')
+    except Exception as e:
+        print(e)
+        return ''
+    return text
+
+
+def extract_text_from_url(url):
+    """
+    Extract text from job description URL.
+    """
+    try:
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        text = ' '.join([p.text for p in soup.find_all('p')])
+    except Exception as e:
+        print(e)
+        return ''
+    return text
+
+
+def extract_keywords(text):
+    """
+    Extract keywords from text.
+    """
+    words = nltk.word_tokenize(text)
+    words = [word.lower() for word in words if word.isalpha()]
+    words = [word for word in words if word not in stopwords.words('english')]
+    return Counter(words)
+
+
+@app.callback(
+    Output('cv-text', 'value'),
+    Input('upload-cv', 'contents'),
+    State('upload-cv', 'filename')
+)
+def update_cv_text(contents, filename):
+    """
+    Update CV text when a file is uploaded.
+    """
+    if contents:
+        text = extract_text_from_cv(contents, filename)
+        return text
+    return ''
+
+
+@app.callback(
+    Output('radar-graph', 'figure'),
+    Input('analyze-button', 'n_clicks'),
+    State('cv-text', 'value'),
+    State('job-description', 'value')
+)
+def update_radar_graph(n_clicks, cv_text, job_description):
+    """
+    Update radar graph when the analyze button is clicked.
+    """
+    if n_clicks > 0:
+        if 'http' in job_description:
+            job_description = extract_text_from_url(job_description)
+        cv_keywords = extract_keywords(cv_text)
+        job_keywords = extract_keywords(job_description)
+        common_keywords = cv_keywords & job_keywords
+        fig = go.Figure()
+        fig.add_trace(go.Scatterpolar(
+            r=[common_keywords[keyword] for keyword in common_keywords],
+            theta=list(common_keywords.keys()),
+            fill='toself',
+            name='CV'
+        ))
+        fig.add_trace(go.Scatterpolar(
+            r=[job_keywords[keyword] for keyword in common_keywords],
+            theta=list(common_keywords.keys()),
+            fill='toself',
+            name='Job Description'
+        ))
+        fig.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    visible=True,
+                    range=[0, 5]
+                )
+            ),
+            showlegend=True
+        )
+        return fig
+    return go.Figure()
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
