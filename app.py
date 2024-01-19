129de0fd711808b7d69d691f31ac77ee3b7c482a
twelsh <twelsh37@gmail.com>
2024-01-19 10:35:39 +0000

    Fixed typo in headwe

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ###############################################################################
-#   This program compars two inputs and returns a similarity score.           #
+#   This program compares two inputs and returns a similarity score.           #
 #    Copyright (C) 2024  Tom Welsh twelsh37@gmail.com                         #
 #                                                                             #
 #    This program is free software: you can redistribute it and/or modify     #
